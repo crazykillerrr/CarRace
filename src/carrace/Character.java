d9bd16b3aa61865a4ff28d4ee22d3244129573f0
dhimas dzulfajri <151392352+crazykillerrr@users.noreply.github.com>
2024-12-12 18:44:38 +0700

    interface

---
+++ src/carrace/Character.java
@@ -0,0 +1,13 @@
+package carrace;
+
+public interface Character {
+    void move(double deltaX, double deltaY);
+    void preventOutOfBounds();
+    double getX();
+    double getY();
+}
+
+
+/*
+ * interface
+ */
