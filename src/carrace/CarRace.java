f0c712ed974058356bd6862beae96f8c5ebeac83
dhimas dzulfajri <151392352+crazykillerrr@users.noreply.github.com>
2024-12-12 18:28:35 +0700

    fix

---
+++ src/carrace/CarRace.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
+ */
+package carrace;
+
+import java.util.Random;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author ddzul
+ */
+public class CarRace extends Application {
+
+    private static final Random random = new Random();
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
